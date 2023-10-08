27225716ae149130022bc94fcbd7b523bb92bfea
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2023-10-08 00:27:06 -0500

    Agrega código para verificar palíndromos en C#

---
+++ exercises/1-exercise-palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string word)
+    {
+        word = word.ToLower().Replace(" ", "");
+        string reversedWord = ReverseString(word);
+        return word == reversedWord;
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main()
+    {
+        // Ejemplos de palabras y si son palíndromos o no
+        Console.WriteLine("Abba: " + IsPalindrome("Abba"));  // Palíndromo
+        Console.WriteLine("Reconocer: " + IsPalindrome("Reconocer"));  // Palíndromo
+        Console.WriteLine("Amo la paloma: " + IsPalindrome("Amo la paloma"));  // No es un palíndromo
+        Console.WriteLine("Hola Mundo: " + IsPalindrome("Hola Mundo"));  // No es un palíndromo
+    }
+}
+
